c7147ef749fcb535a10e20c0856270b9eb0323d1
pjm529 <pok_nima@naver.com>
2022-01-04 10:10:58 +0900

    Feat: 회원 가입 페이지 구현

---
+++ library/src/main/java/library/member/controller/LoginController.java
@@ -0,0 +1,18 @@
+package library.member.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/member")
+public class LoginController {
+
+	// 로그인 페이지 진입
+	@GetMapping("/login.do")
+	public String loginGET() {
+
+		return "member/sub1/login.jsp";
+
+	}
+}
