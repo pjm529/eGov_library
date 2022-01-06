9c2f52f270fcbe7ab574d203fbc2f60d8cf6345d
pjm529 <pok_nima@naver.com>
2022-01-06 09:16:48 +0900

    Add: DateVO

---
+++ library/src/main/java/library/common/domain/DateVO.java
@@ -0,0 +1,24 @@
+package library.common.domain;
+
+public class DateVO {
+
+	private String year;
+	private String month;
+
+	public String getYear() {
+		return year;
+	}
+
+	public void setYear(String year) {
+		this.year = year;
+	}
+
+	public String getMonth() {
+		return month;
+	}
+
+	public void setMonth(String month) {
+		this.month = month;
+	}
+
+}
