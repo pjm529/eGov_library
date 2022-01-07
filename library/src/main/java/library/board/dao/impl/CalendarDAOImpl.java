a974f2397c5db1981178fda098f59de9af4b741f
pjm529 <pok_nima@naver.com>
2022-01-07 14:24:52 +0900

    Add: CalendarDAO

---
+++ library/src/main/java/library/board/dao/impl/CalendarDAOImpl.java
@@ -0,0 +1,11 @@
+package library.board.dao.impl;
+
+import org.springframework.stereotype.Repository;
+
+import egovframework.rte.psl.dataaccess.EgovAbstractMapper;
+import library.board.dao.CalendarDAO;
+
+@Repository
+public class CalendarDAOImpl extends EgovAbstractMapper implements CalendarDAO {
+
+}
