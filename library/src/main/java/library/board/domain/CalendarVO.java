68c416fedcc2fc27c15e1d8ad3e36ae4dfa8e870
pjm529 <pok_nima@naver.com>
2022-01-07 16:07:50 +0900

    Update: Lombok 적용

--- library/src/main/java/library/board/domain/CalendarVO.java
+++ library/src/main/java/library/board/domain/CalendarVO.java
@@ -1,5 +1,8 @@
 package library.board.domain;
 
+import lombok.Data;
+
+@Data
 public class CalendarVO {
 	// 일정번호
 	private int calNo;
@@ -33,93 +36,5 @@ public class CalendarVO {
 
 	// 등록일
 	private String regDate;
-
-	public int getCalNo() {
-		return calNo;
-	}
-
-	public void setCalNo(int calNo) {
-		this.calNo = calNo;
-	}
-
-	public String getGroupId() {
-		return groupId;
-	}
-
-	public void setGroupId(String groupId) {
-		this.groupId = groupId;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public String getUserId() {
-		return userId;
-	}
-
-	public void setUserId(String userId) {
-		this.userId = userId;
-	}
-
-	public String getStart() {
-		return start;
-	}
-
-	public void setStart(String start) {
-		this.start = start;
-	}
-
-	public String getEnd() {
-		return end;
-	}
-
-	public void setEnd(String end) {
-		this.end = end;
-	}
-
-	public boolean isAllDay() {
-		return allDay;
-	}
-
-	public void setAllDay(boolean allDay) {
-		this.allDay = allDay;
-	}
-
-	public String getTextColor() {
-		return textColor;
-	}
-
-	public void setTextColor(String textColor) {
-		this.textColor = textColor;
-	}
-
-	public String getBackgroundColor() {
-		return backgroundColor;
-	}
-
-	public void setBackgroundColor(String backgroundColor) {
-		this.backgroundColor = backgroundColor;
-	}
-
-	public String getBorderColor() {
-		return borderColor;
-	}
-
-	public void setBorderColor(String borderColor) {
-		this.borderColor = borderColor;
-	}
-
-	public String getRegDate() {
-		return regDate;
-	}
-
-	public void setRegDate(String regDate) {
-		this.regDate = regDate;
-	}
 
 }
