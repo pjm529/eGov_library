32924ec1e2d30a82b0962995b4ad460c1343957e
pjm529 <pok_nima@naver.com>
2022-01-07 14:24:41 +0900

    Add: CalendarService

---
+++ library/src/main/java/library/board/service/impl/CalendarServiceImpl.java
@@ -0,0 +1,10 @@
+package library.board.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import library.board.service.CalendarService;
+
+@Service
+public class CalendarServiceImpl implements CalendarService {
+
+}
