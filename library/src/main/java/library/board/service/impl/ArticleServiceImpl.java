eb12821b5a411ec58e9a33b2aa719578a9f9f815
pjm529 <pok_nima@naver.com>
2022-01-10 10:34:20 +0900

    Add: ArticleService

---
+++ library/src/main/java/library/board/service/impl/ArticleServiceImpl.java
@@ -0,0 +1,10 @@
+package library.board.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import library.board.service.ArticleService;
+
+@Service
+public class ArticleServiceImpl implements ArticleService {
+
+}
