b53d686a96bfdf1c12c5952314d00b265b17b57e
pjm529 <pok_nima@naver.com>
2022-01-11 09:33:30 +0900

    Add: QnaService

---
+++ library/src/main/java/library/board/service/impl/QnaServiceImpl.java
@@ -0,0 +1,10 @@
+package library.board.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import library.board.service.QnaService;
+
+@Service
+public class QnaServiceImpl implements QnaService {
+
+}
