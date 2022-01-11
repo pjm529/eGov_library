148b56d776d8fa6fbcac535dbb76edfb6396434e
pjm529 <pok_nima@naver.com>
2022-01-11 09:33:34 +0900

    Add: QnaDAO

---
+++ library/src/main/java/library/board/dao/impl/QnaDAOImpl.java
@@ -0,0 +1,11 @@
+package library.board.dao.impl;
+
+import org.springframework.stereotype.Repository;
+
+import egovframework.rte.psl.dataaccess.EgovAbstractMapper;
+import library.board.dao.QnaDAO;
+
+@Repository
+public class QnaDAOImpl extends EgovAbstractMapper implements QnaDAO {
+
+}
