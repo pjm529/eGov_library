a192b708965ff87e46c791f5389dd0f42baf7baf
pjm529 <pok_nima@naver.com>
2022-01-11 09:40:35 +0900

    Add: AnswerVO

---
+++ library/src/main/java/library/board/domain/AnswerVO.java
@@ -0,0 +1,34 @@
+package library.board.domain;
+
+import java.sql.Timestamp;
+
+import lombok.Data;
+
+@Data
+public class AnswerVO {
+	
+	// 답변 번호
+	private Long answerNo;
+	
+	// 문의사항 번호
+	private Long enquiryNo;
+	
+	// 답변 제목
+	private String answerTitle;
+	
+	// 답변 내용
+	private String answerContent;
+	
+	// 답변 작성자 ID
+	private String aWriterId;
+	
+	// 답변 작성자명
+	private String aWriterName;
+	
+	// 답변 조회수
+	private int answerHits;
+	
+	// 답변 작성일
+	private Timestamp answerRegDate;
+
+}
