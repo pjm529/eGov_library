628e0f27a07c0fd864ad23cd197ad20fc4798d81
pjm529 <pok_nima@naver.com>
2022-01-11 10:53:46 +0900

    Add: AnswerService

---
+++ library/src/main/java/library/board/service/impl/AnswerServiceImpl.java
@@ -0,0 +1,10 @@
+package library.board.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import library.board.service.AnswerService;
+
+@Service
+public class AnswerServiceImpl implements AnswerService {
+
+}
