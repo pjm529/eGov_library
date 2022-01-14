d4a3d0296119f1f5a6544a2708aee531b1e1a021
pjm529 <pok_nima@naver.com>
2022-01-14 09:31:31 +0900

    Add: HopeService

---
+++ library/src/main/java/library/mylib/service/impl/HopeServiceImpl.java
@@ -0,0 +1,10 @@
+package library.mylib.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import library.mylib.service.HopeService;
+
+@Service
+public class HopeServiceImpl implements HopeService {
+
+}
