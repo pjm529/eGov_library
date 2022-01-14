6dbdf2227f0731c854d12faf668a2ca342833173
pjm529 <pok_nima@naver.com>
2022-01-14 14:55:52 +0900

    Add: MasterService

---
+++ library/src/main/java/library/admin/service/impl/MasterServiceImpl.java
@@ -0,0 +1,10 @@
+package library.admin.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import library.admin.service.MasterService;
+
+@Service
+public class MasterServiceImpl implements MasterService {
+
+}
