26945f9471395d99b1a5691b341536e6c64350c1
pjm529 <pok_nima@naver.com>
2022-01-14 09:30:56 +0900

    Add: HopeDAO

---
+++ library/src/main/java/library/mylib/dao/impl/HopeDAOImpl.java
@@ -0,0 +1,11 @@
+package library.mylib.dao.impl;
+
+import org.springframework.stereotype.Repository;
+
+import egovframework.rte.psl.dataaccess.EgovAbstractMapper;
+import library.mylib.dao.HopeDAO;
+
+@Repository
+public class HopeDAOImpl extends EgovAbstractMapper implements HopeDAO {
+
+}
