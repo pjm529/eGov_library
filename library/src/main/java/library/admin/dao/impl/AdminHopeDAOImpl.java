4ee4574cf0c6b1a3887e1fe4718b6e64784d8aa4
pjm529 <pok_nima@naver.com>
2022-01-14 10:40:10 +0900

    Add: AdminHopeDAO

---
+++ library/src/main/java/library/admin/dao/impl/AdminHopeDAOImpl.java
@@ -0,0 +1,10 @@
+package library.admin.dao.impl;
+
+import org.springframework.stereotype.Repository;
+
+import library.admin.dao.AdminHopeDAO;
+
+@Repository
+public class AdminHopeDAOImpl implements AdminHopeDAO {
+
+}
