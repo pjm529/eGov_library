92f46f8300ddc2c2d6ad6e5561dc6942810f4c11
pjm529 <pok_nima@naver.com>
2022-01-14 13:54:40 +0900

    Add: BannerDAO

---
+++ library/src/main/java/library/admin/dao/impl/BannerDAOImpl.java
@@ -0,0 +1,11 @@
+package library.admin.dao.impl;
+
+import org.springframework.stereotype.Repository;
+
+import egovframework.rte.psl.dataaccess.EgovAbstractMapper;
+import library.admin.dao.BannerDAO;
+
+@Repository
+public class BannerDAOImpl extends EgovAbstractMapper implements BannerDAO {
+
+}
