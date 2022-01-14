f701b3329b23b384ca242147b6725ddf029b46ec
pjm529 <pok_nima@naver.com>
2022-01-14 10:40:17 +0900

    Add: AdminHopeService

---
+++ library/src/main/java/library/admin/service/impl/AdminHopeServiceImpl.java
@@ -0,0 +1,10 @@
+package library.admin.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import library.admin.service.AdminHopeService;
+
+@Service
+public class AdminHopeServiceImpl implements AdminHopeService {
+
+}
