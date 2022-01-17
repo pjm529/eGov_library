9ce553e3a20158079264abfcad3651cd777c8eb4
pjm529 <pok_nima@naver.com>
2022-01-17 14:21:37 +0900

    Add: ReserveService

---
+++ library/src/main/java/library/mylib/service/impl/ReserveServiceImpl.java
@@ -0,0 +1,10 @@
+package library.mylib.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import library.mylib.service.ReserveService;
+
+@Service
+public class ReserveServiceImpl implements ReserveService {
+
+}
