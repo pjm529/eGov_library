30cbcd2fe77c09a600cc67bb61a6bf47137fdc74
pjm529 <pok_nima@naver.com>
2022-01-17 17:38:00 +0900

    Feat: 관리자 이용 중인 열람실 퇴실 기능 구현

--- library/src/main/java/library/admin/controller/AdminRoomController.java
+++ library/src/main/java/library/admin/controller/AdminRoomController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import library.mylib.domain.RoomVO;
@@ -14,18 +16,29 @@ import library.mylib.service.RoomService;
 @Controller
 @RequestMapping("/admin")
 public class AdminRoomController {
-	
+
 	@Autowired
 	private RoomService roomService;
 
+	// 열람실 이용 정보
 	@GetMapping("/seatList.do")
 	public ModelAndView seatList() {
-		
+
 		ModelAndView mav = new ModelAndView("admin/sub6/seatList.jsp");
-		
+
+		// 열람실 이용 중인 좌석 정보
 		List<RoomVO> seatList = roomService.seatList();
 		mav.addObject("seatList", seatList);
-		
+
 		return mav;
+	}
+
+	// 퇴실
+	@PostMapping("/seatReturn.do")
+	public String seat_return(@RequestParam String userId) {
+
+		roomService.returnSeat(userId);
+
+		return "redirect:/admin/seatList.do";
 	}
 }
