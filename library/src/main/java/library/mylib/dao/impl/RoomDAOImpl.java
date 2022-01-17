d1782c7300b0aa494cf656a567bd053d5dac7d0d
pjm529 <pok_nima@naver.com>
2022-01-17 11:26:08 +0900

    Add: RoomDAO

---
+++ library/src/main/java/library/mylib/dao/impl/RoomDAOImpl.java
@@ -0,0 +1,11 @@
+package library.mylib.dao.impl;
+
+import org.springframework.stereotype.Repository;
+
+import egovframework.rte.psl.dataaccess.EgovAbstractMapper;
+import library.mylib.dao.RoomDAO;
+
+@Repository
+public class RoomDAOImpl extends EgovAbstractMapper implements RoomDAO {
+
+}
