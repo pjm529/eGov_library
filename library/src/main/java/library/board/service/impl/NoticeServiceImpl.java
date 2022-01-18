1ce34c25cc78e2e75c5d6196d54db29672624e94
pjm529 <pok_nima@naver.com>
2022-01-18 13:18:49 +0900

    Add: NoticeService

---
+++ library/src/main/java/library/board/service/impl/NoticeServiceImpl.java
@@ -0,0 +1,10 @@
+package library.board.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import library.board.service.NoticeService;
+
+@Service
+public class NoticeServiceImpl implements NoticeService {
+
+}
