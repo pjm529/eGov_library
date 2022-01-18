e93b9118d0212e6c7b6bad082e0a5f3dab6228b3
pjm529 <pok_nima@naver.com>
2022-01-18 14:12:34 +0900

    Add: NoticeAttachService

---
+++ library/src/main/java/library/board/service/impl/NoticeAttachServiceImpl.java
@@ -0,0 +1,10 @@
+package library.board.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import library.board.service.NoticeAttachService;
+
+@Service
+public class NoticeAttachServiceImpl implements NoticeAttachService {
+
+}
