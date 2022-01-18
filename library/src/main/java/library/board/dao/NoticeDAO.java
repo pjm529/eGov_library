87e22d7d011745d9b3fa28d7d6c28a77efd9e9c1
pjm529 <pok_nima@naver.com>
2022-01-18 13:18:15 +0900

    Add: NoticeDAO

---
+++ library/src/main/java/library/board/dao/NoticeDAO.java
@@ -0,0 +1,5 @@
+package library.board.dao;
+
+public interface NoticeDAO {
+
+}
---
+++ library/src/main/java/library/board/dao/impl/NoticeDAOImpl.java
@@ -0,0 +1,11 @@
+package library.board.dao.impl;
+
+import org.springframework.stereotype.Repository;
+
+import egovframework.rte.psl.dataaccess.EgovAbstractMapper;
+import library.board.dao.NoticeDAO;
+
+@Repository
+public class NoticeDAOImpl extends EgovAbstractMapper implements NoticeDAO {
+
+}
