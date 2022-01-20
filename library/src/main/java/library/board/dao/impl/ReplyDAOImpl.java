6a657e8880fe7927cb469910db733d0133b8d2c8
pjm529 <pok_nima@naver.com>
2022-01-20 16:20:55 +0900

    Add: ReplyDAO

---
+++ library/src/main/java/library/board/dao/impl/ReplyDAOImpl.java
@@ -0,0 +1,11 @@
+package library.board.dao.impl;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import egovframework.rte.psl.dataaccess.EgovAbstractMapper;
+import library.board.dao.ReplyDAO;
+
+@RequestMapping
+public class ReplyDAOImpl extends EgovAbstractMapper implements ReplyDAO {
+
+}
